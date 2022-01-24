d5ff7218faf5efa588dbad909b608103ea25ee6f
Martina Thege <martinathege@gmail.com>
2022-01-24 16:07:25 +0100

    Fix

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './contact.scss';
 import personImage from '../../images/personimage.png';
-
+import {FaGithub, FaInstagram, FaLinkedin} from 'react-icons/fa';
+import { IconContext } from 'react-icons';
 
 export default function Contact() {
 
     return (
+      <IconContext.Provider value={{ size: '3em', color: 'black'}}>
         <div className="contact" id="contact">
           <div className="contact-container">
             <div className="contact-header">Kontakt</div>
@@ -14,12 +15,17 @@ export default function Contact() {
           </div> 
 
           <div className="contact-info">
-<div className="contact-info-pb"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis nisi corrupti similique natus id saepe unde voluptatem. Maiores sapiente id optio ullam debitis deserunt est provident necessitatibus saepe facere. <br/> <br/> Dicta voluptatibus magnam neque molestias. Nesciunt facere ratione facilis tenetur dicta sed incidunt similique inventore eius! Quibusdam consequuntur tempora quidem recusandae consectetur suscipit, labore eligendi laudantium veritatis perspiciatis. <br/> <br/> Magnam nesciunt debitis in, optio, quod vel nobis ea minus recusandae explicabo, voluptates accusantium dicta.<br/> <br/>  Quae molestiae, itaque deleniti a neque aspernatur soluta adipisci amet saepe magnam ad ratione nesciunt eligendi asperiores rerum eaque sapiente laboriosam aliquam tempore nam laudantium quos doloremque eveniet.</div>
+<div className="contact-info-pb"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis nisi corrupti similique natus id saepe unde voluptatem. Maiores sapiente id optio ullam debitis deserunt est provident necessitatibus saepe facere. <br/> <br/> Dicta voluptatibus magnam neque molestias. Nesciunt facere ratione facilis tenetur dicta sed incidunt similique inventore eius! Quibusdam consequuntur tempora quidem recusandae consectetur suscipit, labore eligendi laudantium veritatis perspiciatis. <br/> <br/> Magnam nesciunt debitis in, optio, quod vel nobis ea minus recusandae explicabo, voluptates accusantium dicta.</div>
 <div className="contact-info-adress">Martina Thege <br/>
 Ljusnevägen 26 <br/>
 12848 Bagarmossen <br/>
 +46 70 488 08 67<br/>
-info@detomdetta.se </div>
+<u>info@detomdetta.se</u>
+<div className="icons">
+  
+<a href="https://github.com/MartinaThege"> <FaGithub/></a> <a href="https://instagram.se/det.om.detta"> <FaInstagram/></a> <a href="https://www.linkedin.com/in/martina-thege-0b7466b7/?originalSubdomain=se"> <FaLinkedin/></a></div>
+
+</div>
 <div className="contact-info-image">
 <img src={personImage}/>
 </div>
@@ -27,5 +33,6 @@ info@detomdetta.se </div>
           </div>
               
         </div>
+        </IconContext.Provider> 
     )
 }
